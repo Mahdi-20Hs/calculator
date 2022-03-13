485951d91f86117b2f5630a179649aaad884a26e
Mahdi <mahdihs395@gmail.com>
2022-03-13 21:03:12 +0300

    created functions that populate the display

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+const display = document.querySelector('.content');
+const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
+const divideButton = document.querySelector('.divide');
+const multiplyButton = document.querySelector('.multiply');
+const subtractButton = document.querySelector('.subtract');
+const addButton = document.querySelector('.add');
+const equalButton = document.querySelector('.equal');
+const pointButton = document.querySelector('.point');
+const zero = document.querySelector('.zero');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+
+function populateDisply(button){
+  button.addEventListener('click', () => {
+    if(display.textContent === '0'){
+      display.textContent = '';
+    }
+    display.textContent += button.textContent;
+  })
+}
+
+populateDisply(zero);
+populateDisply(one);
+populateDisply(two);
+populateDisply(three);
+populateDisply(four);
+populateDisply(five);
+populateDisply(six);
+populateDisply(seven);
+populateDisply(eight);
+populateDisply(nine);
+populateDisply(pointButton);
+populateDisply(addButton);
+populateDisply(subtractButton);
+populateDisply(multiplyButton);
+populateDisply(divideButton)
+clearButton.addEventListener('click', () => {
+  display.textContent = '';
+});
+deleteButton.addEventListener('click', () => {
+  let splitedDisplay = display.textContent.split("");
+  splitedDisplay.splice(display.textContent.length - 1, 1);
+  let result = splitedDisplay.join('');
+  display.textContent = result;
+})
+
+
+
+
 function add(num1, num2){
   return Number(num1) + Number(num2);
 }
