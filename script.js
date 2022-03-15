a3aaa0a1dfee3d9cd27721a20378bda7a8acbbcd
Mahdi <mahdihs395@gmail.com>
2022-03-15 18:05:19 +0300

    created functions for delete and clear and edited populateDisplay logic and made results with long decimals rounded

--- script.js
+++ script.js
@@ -18,45 +18,82 @@ const seven = document.querySelector('.seven');
 const eight = document.querySelector('.eight');
 const nine = document.querySelector('.nine');
 
-function populateDisply(button){
+
+function deleteLastChar(){
+  let splitedDisplay = display.textContent.split("");
+  if(display.textContent.charAt(display.textContent.length - 1) === ' '){
+    splitedDisplay.splice(display.textContent.length - 3, 3);
+  }else{
+    splitedDisplay.splice(display.textContent.length - 1, 1);
+  }
+  let result = splitedDisplay.join('');
+  display.textContent = result;
+}
+function populateDisplay(button){
   button.addEventListener('click', () => {
-    if(display.textContent === '0'){
+    let displayText = display.textContent;
+    if(displayText === '0' && button !== pointButton){
       display.textContent = '';
     }
-    display.textContent += button.textContent;
+
+    if(
+      (button === addButton || button === subtractButton || button === multiplyButton || button === divideButton) &&
+      (displayText.charAt(displayText.length - 2) === '+' ||
+      displayText.charAt(displayText.length - 2) === '-' ||
+      displayText.charAt(displayText.length - 2) === '×' ||
+      displayText.charAt(displayText.length - 2) === '÷')){
+        deleteLastChar();
+      }
+      
+    if(button === addButton || button === subtractButton || button === multiplyButton || button === divideButton){
+      if(display.textContent.split(" ")[2] !== undefined){
+        display.textContent = `${operation(display.textContent.split(" ")[1], Number(display.textContent.split(" ")[0]), Number(display.textContent.split(" ")[2]))}`
+      }
+      display.textContent += ` ${button.textContent} `;
+    }else{
+      display.textContent += button.textContent;
+    }
+    if(display.textContent.split(" ")[1] === undefined && display.textContent.split(" ")[0].includes('.')){
+      pointButton.setAttribute('disabled', true)
+    }else if(display.textContent.split(" ")[1] !== undefined && display.textContent.split(" ")[2].includes('.')){
+      pointButton.setAttribute('disabled', true)
+    }else{
+      pointButton.toggleAttribute('disabled', false)
+    }
+    
   })
 }
 
-populateDisply(zero);
-populateDisply(one);
-populateDisply(two);
-populateDisply(three);
-populateDisply(four);
-populateDisply(five);
-populateDisply(six);
-populateDisply(seven);
-populateDisply(eight);
-populateDisply(nine);
-populateDisply(pointButton);
-populateDisply(addButton);
-populateDisply(subtractButton);
-populateDisply(multiplyButton);
-populateDisply(divideButton)
+populateDisplay(zero);
+populateDisplay(one);
+populateDisplay(two);
+populateDisplay(three);
+populateDisplay(four);
+populateDisplay(five);
+populateDisplay(six);
+populateDisplay(seven);
+populateDisplay(eight);
+populateDisplay(nine);
+populateDisplay(pointButton);
+populateDisplay(addButton);
+populateDisplay(subtractButton);
+populateDisplay(multiplyButton);
+populateDisplay(divideButton)
 clearButton.addEventListener('click', () => {
   display.textContent = '';
 });
-deleteButton.addEventListener('click', () => {
-  let splitedDisplay = display.textContent.split("");
-  splitedDisplay.splice(display.textContent.length - 1, 1);
-  let result = splitedDisplay.join('');
-  display.textContent = result;
+deleteButton.addEventListener('click', deleteLastChar)
+equalButton.addEventListener('click', () => {
+  if(display.textContent.split(" ").length === 3){
+    display.textContent = `${operation(display.textContent.split(" ")[1], Number(display.textContent.split(" ")[0]), Number(display.textContent.split(" ")[2]))}`
+  }
 })
 
 
 
 
 function add(num1, num2){
-  return Number(num1) + Number(num2);
+  return (num1 + num2);
 }
 
 function subtract(num1, num2){
@@ -68,17 +105,21 @@ function multiply(num1, num2){
 }
 
 function divide(num1, num2){
+  if(num2 === 0){
+    return alert('don\'t divide by zero')
+  }
   return num1 / num2
 }
 
 function operation(operator, num1, num2){
+  
   if(operator === '+'){
-     return add(num1, num2)
+     return add(num1, num2).toFixed(2)
   }else if(operator === '-'){
-    return subtract(num1, num2)
-  }else if(operator === '*'){
-    return multiply(num1, num2)
-  }else if(operator === '/'){
-    return divide(num1, num2)
+    return subtract(num1, num2).toFixed(2)
+  }else if(operator === '×'){
+    return multiply(num1, num2).toFixed(2)
+  }else if(operator === '÷'){
+    return divide(num1, num2).toFixed(2)
   }
 }
